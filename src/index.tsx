1f8e244f161d5b58b706fdb62b681c4d7870e9b4
Marisa D. Richmond <marisa.richmond94@gmail.com>
2023-07-22 13:41:20 -0700

    More Initial Set up
    - Remove redux (I would prefer to use built-in providers
    - A few more changes related to logos and index.html set up
    - Start setting up auth0 integration
    - Configure TS settings

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,32 @@
+import { Auth0Provider } from '@auth0/auth0-react';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './store';
-import reportWebVitals from './reportWebVitals';
 
-const container = document.getElementById('root')!;
-const root = createRoot(container);
+import auth0Config from 'config/auth0.json';
+import reportWebVitals from 'reportWebVitals';
+import history from "utils/history";
+
+const onRedirectCallback = (appState: any) => {
+  history.push(
+    appState && appState.returnTo ? appState.returnTo : window.location.pathname
+  );
+};
+
+const root = createRoot(document.getElementById('root')!);
+
+const auth0ProviderConfig = {
+  ...auth0Config,
+  onRedirectCallback,
+  authorizationParams: {
+    redirect_uri: window.location.origin,
+  }
+}
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-    </Provider>
+    <Auth0Provider {...auth0ProviderConfig}>
+    </Auth0Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
